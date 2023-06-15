797f93bbd842f7ddb59d3acb14e68c461d3ad5b8
Caroline Ferraz <projetos.crrz@gmail.com>
2023-06-15 20:29:03 -0300

    Feat: Criando método construtor na entidade Usuario

--- src/main/java/com/estantedelivros/api/Domain/Usuario.java
+++ src/main/java/com/estantedelivros/api/Domain/Usuario.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,7 +16,22 @@ public class Usuario {
     private UUID id;
     private String nome;
     private String email;
+    private String senha;
     private List<Categoria> trofeis;
     private List<UUID> livrosLidos; // lista com os id's dos livros que o usuário já leu
     private int pontuacao;
+
+    public Usuario(String nome, String email, String senha){
+        this.setId();
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.trofeis = new ArrayList<Categoria>();
+        this.livrosLidos = new ArrayList<UUID>();
+        this.pontuacao = 0;
+    }
+
+    protected void setId(){
+        this.id = UUID.randomUUID();
+    }
 }
