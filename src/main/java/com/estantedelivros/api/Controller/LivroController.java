0cd895ca5de79c3f9071e0b9f4617fa13460eb4c
Caroline Ferraz <projetos.crrz@gmail.com>
2023-06-14 21:03:19 -0300

    Feat: Criando a classe LivroController

---
+++ src/main/java/com/estantedelivros/api/Controller/LivroController.java
@@ -0,0 +1,33 @@
+package com.estantedelivros.api.Controller;
+
+import com.estantedelivros.api.Service.Livro.DadosCadastroDeLivro;
+import com.estantedelivros.api.Service.Livro.DadosListagemDeLivros;
+import com.estantedelivros.api.Service.Livro.ILivroService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/livro")
+public class LivroController {
+
+    @Autowired
+    private ILivroService _livroService;
+
+    @PostMapping
+    public ResponseEntity<String> cadastrarLivro(@RequestBody @Valid DadosCadastroDeLivro dados){
+        var resposta = _livroService.cadastrarLivro(dados);
+        return ResponseEntity.status(HttpStatus.CREATED).body(resposta);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemDeLivros>> listarLivros(@PageableDefault(size = 10, sort = {"titulo"}) Pageable pageable){
+        var resposta = _livroService.listarLivros(pageable);
+        return ResponseEntity.ok(resposta);
+    }
+}
