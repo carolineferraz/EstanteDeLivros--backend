768ff94d371ea3d6904827e24196e173b6526f2d
Caroline Ferraz <projetos.crrz@gmail.com>
2023-06-14 21:01:11 -0300

    Feat: Adicionando construtor para cadastrar novo livro a partir dos dados do DTO e adicionando anotação @Enumerated ao Enum Categoria

--- src/main/java/com/estantedelivros/api/Domain/Livro.java
+++ src/main/java/com/estantedelivros/api/Domain/Livro.java
@@ -1,10 +1,12 @@
 package com.estantedelivros.api.Domain;
 
-import com.estantedelivros.api.Domain.Categoria;
+import com.estantedelivros.api.Service.Livro.DadosCadastroDeLivro;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import java.util.UUID;
 
 @Data
@@ -16,6 +18,21 @@ public class Livro {
     private String titulo;
     private String descricao;
     private int qtdPaginas;
+
+    @Enumerated(EnumType.STRING)
     private Categoria categoria;
     private String foto;
+
+    public Livro(String titulo, String descricao, int qtdPaginas, Categoria categoria, String foto) {
+        this.setId();
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.qtdPaginas = qtdPaginas;
+        this.categoria = categoria;
+        this.foto = foto;
+    }
+
+    protected void setId(){
+        this.id = UUID.randomUUID();
+    }
 }
