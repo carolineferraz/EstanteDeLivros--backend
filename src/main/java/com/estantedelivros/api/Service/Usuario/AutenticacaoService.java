9d1f11677821d013f32d24ac52062b40d93af1eb
Caroline Ferraz <projetos.crrz@gmail.com>
2023-06-19 22:20:11 -0300

    Feat: Criando a classe AutenticacaoService

---
+++ src/main/java/com/estantedelivros/api/Service/Usuario/AutenticacaoService.java
@@ -0,0 +1,20 @@
+package com.estantedelivros.api.Service.Usuario;
+
+import com.estantedelivros.api.Infrastructure.IUsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    @Autowired
+    private IUsuarioRepository _usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return _usuarioRepository.findByEmail(username);
+    }
+}
