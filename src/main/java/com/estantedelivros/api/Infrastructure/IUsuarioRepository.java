9f5c0846fba68bac8423b403cf7711083fcffcae
Caroline Ferraz <projetos.crrz@gmail.com>
2023-06-19 22:19:34 -0300

    Feat: Criando query method findByEmail()

--- src/main/java/com/estantedelivros/api/Infrastructure/IUsuarioRepository.java
+++ src/main/java/com/estantedelivros/api/Infrastructure/IUsuarioRepository.java
@@ -2,8 +2,11 @@ package com.estantedelivros.api.Infrastructure;
 
 import com.estantedelivros.api.Domain.Usuario;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.UUID;
 
 public interface IUsuarioRepository extends MongoRepository<Usuario, UUID> {
+
+    UserDetails findByEmail(String email);
 }
