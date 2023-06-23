cbf0977852291d8187848cd47a1431bba058b715
Caroline Ferraz <projetos.crrz@gmail.com>
2023-06-22 21:01:59 -0300

    Feat: Adicionando a dependência java-jwt ao pom.xml e adicionando o token jwt como retorno da requisição POST para autenticar o usuário (ainda faltando ajustes)

--- src/main/java/com/estantedelivros/api/Controller/AutenticacaoController.java
+++ src/main/java/com/estantedelivros/api/Controller/AutenticacaoController.java
@@ -1,5 +1,7 @@
 package com.estantedelivros.api.Controller;
 
+import com.estantedelivros.api.Domain.Usuario;
+import com.estantedelivros.api.Infrastructure.Security.TokenService;
 import com.estantedelivros.api.Service.Autenticacao.DadosAutenticacao;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +20,14 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager _authenticationManager;
 
+    @Autowired
+    private TokenService _tokenService;
+
     @PostMapping
     public ResponseEntity autenticarUsuario(@RequestBody @Valid DadosAutenticacao dados){
         var token = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
         var autentication = _authenticationManager.authenticate(token);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(_tokenService.gerarToken((Usuario) autentication.getPrincipal()));
     }
 }
